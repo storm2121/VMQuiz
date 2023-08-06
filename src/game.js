d7c0c319b28b7ac63e39ffa24cd0f85659160e40
storm2121 <44982157+storm2121@users.noreply.github.com>
2023-08-06 18:31:25 +0100

    Revert "Dropdown menu for answers"
    This reverts commit f66dcd3fba69698dca605699d0ea1e176f306187.

--- src/game.js
+++ src/game.js
@@ -12,30 +12,11 @@ function Game() {
   const [currentSongGame, setCurrentSongGame] = useState("");
   const [guess, setGuess] = useState("");
   const [members, setMembers] = useState([]);
-  const [guessTime, setGuessTime] = useState(0);
-  const [gameNames, setGameNames] = useState([]);
-  const [filteredGameNames, setFilteredGameNames] = useState([]);
   const { lobbyId } = useParams();
   const navigate = useNavigate();
   const audioRef = useRef();
   const gameIntervalRef = useRef();
   const countdownIntervalRef = useRef();
-
-  useEffect(() => {
-    const fetchGameNames = async () => {
-      const gameNamesRef = collection(firestore, '/songs');
-      const gameNamesQuerySnapshot = await getDocs(gameNamesRef);
-      const gameNames = [];
-
-      gameNamesQuerySnapshot.forEach((doc) => {
-        gameNames.push(doc.data().game);
-      });
-
-      setGameNames([...new Set(gameNames)]);
-    };
-
-    fetchGameNames();
-  }, []);
 
   useEffect(() => {
     const initializeMembers = async () => {
@@ -44,15 +25,14 @@ function Game() {
       const membersData = membersSnapshot.val() || {};
       
       for (const userId in membersData) {
-        if (!membersData[userId].points || !membersData[userId].hasGuessed) {
-          await set(ref(db, `/lobbies/${lobbyId}/members/${userId}`), { points: 0, hasGuessed: false });
+        if (!membersData[userId].points) {
+          await set(ref(db, `/lobbies/${lobbyId}/members/${userId}`), { points: 0 });
         }
       }
       
       setMembers(Object.entries(membersData).map(([userId]) => ({
         name: userId,
         points: membersData[userId].points || 0,
-        hasGuessed: membersData[userId].hasGuessed || false,
       })));
     };
 
@@ -64,50 +44,26 @@ function Game() {
       setMembers(Object.entries(membersData).map(([userId, userData]) => ({
         name: userId,
         points: userData.points || 0,
-        hasGuessed: userData.hasGuessed || false,
       })));
     });
 
     const lobbySettingsRef = ref(db, `/lobbies/${lobbyId}/settings`);
     onValue(lobbySettingsRef, async (snapshot) => {
       const settings = snapshot.val();
-      setGuessTime(settings.guessTime);
       const fetchedSongs = await fetchSongs(settings.songGenre);
       if (fetchedSongs.length > 0) {
         startGame(fetchedSongs, settings.guessTime, settings.numSongs);
       }
     });
 
-    const lobbySongsRef = ref(db, `/lobbies/${lobbyId}/currentSong`);
-    onValue(lobbySongsRef, (snapshot) => {
-      const currentSongData = snapshot.val();
-
-      if (currentSongData) {
-        setTimeLeft(guessTime);
-        setCurrentSongUrl(currentSongData.url);
-        setCurrentSongGame(currentSongData.game);
-        setCurrentSongName("???");
-
-        clearInterval(countdownIntervalRef.current);
-        countdownIntervalRef.current = setInterval(() => {
-          setTimeLeft(prevTimeLeft => {
-            if (prevTimeLeft <= 1) {
-              clearInterval(countdownIntervalRef.current);
-              setCurrentSongName(currentSongData.game);
-            }
-            return prevTimeLeft - 1;
-          });
-        }, 1000);
-      }
-    });
-
     return () => {
       off(membersRef);
-      off(lobbySongsRef);
       clearInterval(gameIntervalRef.current);
       clearInterval(countdownIntervalRef.current);
     };
-  }, [lobbyId, navigate, guessTime]);
+  }, [lobbyId, navigate]);
+
+  // ... Rest of the code ...
 
   const fetchSongs = async (songGenre) => {
     const songsRef = collection(firestore, '/songs');
@@ -124,87 +80,80 @@ function Game() {
   };
 
   const startGame = (songs, guessTime, numSongs) => {
-    const lobbySongsRef = ref(db, `/lobbies/${lobbyId}/currentSong`);
-  
     let rounds = 0;
     const playedSongs = new Set();
-    const playRound = async () => {
+    const playRound = () => {
       if (rounds >= numSongs || playedSongs.size >= songs.length) {
         clearInterval(gameIntervalRef.current);
         navigate(`/lobby/${lobbyId}`);
         return;
       }
-  
+
       let randomSong;
       do {
         randomSong = songs[Math.floor(Math.random() * songs.length)];
-      } while (playedSongs.has(randomSong.url));  // use url for comparison
-      playedSongs.add(randomSong.url);  // add url to the set
-  
-      await set(lobbySongsRef, {
-        url: randomSong.url,
-        game: randomSong.game
-      });
-  
-      // reset hasGuessed for all members at the start of each round
-      const membersRef = ref(db, `/lobbies/${lobbyId}/members`);
-      const membersSnapshot = await get(membersRef);
-      const membersData = membersSnapshot.val() || {};
-      
-      for (const userId in membersData) {
-        await set(ref(db, `/lobbies/${lobbyId}/members/${userId}`), { ...membersData[userId], hasGuessed: false });
-      }
-  
+      } while (playedSongs.has(randomSong.url));
+      playedSongs.add(randomSong.url);
+
+      setCurrentSongUrl(randomSong.url);
+      setCurrentSongGame(randomSong.game);
+      setTimeLeft(guessTime);
+
+      clearInterval(countdownIntervalRef.current); // Clear previous interval
+      countdownIntervalRef.current = setInterval(() => {
+        setTimeLeft(prevTimeLeft => {
+          if (prevTimeLeft <= 1) {
+            clearInterval(countdownIntervalRef.current);
+            endRound();
+          }
+          return prevTimeLeft - 1;
+        });
+      }, 1000);
+
       rounds++;
     };
-  
+
     playRound();
-    gameIntervalRef.current = setInterval(playRound, (guessTime + 3) * 1000);
+    gameIntervalRef.current = setInterval(playRound, (guessTime + 5) * 1000);
   };
-  
+
+  const endRound = () => {
+    setCurrentSongName(currentSongGame);
+  };
 
   const submitGuess = async () => {
-    if (guess.toLowerCase() === currentSongGame.toLowerCase()) {
-      const email = auth.currentUser.email;
-      const userId = email.split('@')[0];
-      const userRef = ref(db, `/lobbies/${lobbyId}/members/${userId}`);
-      const userSnapshot = await get(userRef);
-      const userData = userSnapshot.val();
-
-      if (userData) {
-        await set(userRef, {
-          points: (userData.points || 0) + 1,
-          hasGuessed: true,
-        });
+    try {
+      if (auth.currentUser && guess === currentSongGame) {
+        const email = auth.currentUser.email;
+        const userId = email.split('@')[0];
+        const memberRef = ref(db, `/lobbies/${lobbyId}/members/${userId}`);
+        const memberSnapshot = await get(memberRef);
+        const memberData = memberSnapshot.val() || { points: 0 };
+        const memberPoints = memberData.points + 1;
+        await set(memberRef, { points: memberPoints });
       }
-
+  
       setGuess("");
+    } catch (error) {
+      console.error("Error submitting guess:", error);
     }
   };
 
-  const handleGuessChange = (e) => {
-    setGuess(e.target.value);
-    setFilteredGameNames(
-      gameNames.filter(gameName => gameName.toLowerCase().startsWith(e.target.value.toLowerCase()))
-    );
-  };
+  useEffect(() => {
+    if (currentSongUrl) {
+      audioRef.current.load();
+      audioRef.current.play();
+    }
+  }, [currentSongUrl]);
 
   return (
-<div>
+    <div>
       <h1>Guess the Song!</h1>
       <div>Time left: {timeLeft}</div>
-      <input type="text" value={guess} onChange={handleGuessChange} />
-      {
-        filteredGameNames.length > 0 &&
-        <ul>
-          {filteredGameNames.map((gameName, index) => (
-            <li key={index} onClick={() => setGuess(gameName)}>{gameName}</li>
-          ))}
-        </ul>
-      }
+      <input type="text" value={guess} onChange={(e) => setGuess(e.target.value)} />
       <button onClick={submitGuess}>Submit Guess</button>
       <div>Current Song: {currentSongName}</div>
-      <audio ref={audioRef} controls src={currentSongUrl} autoPlay></audio>
+      <audio ref={audioRef} controls src={currentSongUrl}></audio>
       <button onClick={() => navigate(`/lobby/${lobbyId}`)}>Back to Lobby</button>
       <div>
         <h2>Members:</h2>
@@ -213,7 +162,7 @@ function Game() {
         ))}
       </div>
     </div>
-      );
+  );
 }
 
 export default Game;
